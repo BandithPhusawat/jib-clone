b58e337a49488651b019bb1718e0b26cc76a19c9
Mr.NoN <phusawatbandith@gmail.com>
2025-03-14 10:34:40 +0700

    chage page to  page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,24 @@ import { CFooter } from "@/svg";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-[1920px] mx-auto h-screen space-y-10">
-      <div className="fixed w-full max-w-[1920px] mx-auto z-50">
+    <div className="min-w-[1920px] w-full h-dvh space-y-10">
+      <div className="fixed w-full z-50">
         <HeaderTitle />
         <Header />
       </div>
-      <div className="w-full max-w-[1920px] mx-auto pt-24">
+      <div className="w-full pt-24">
         <BannerSide />
         <Content />
       </div>
-      <div className="bg-[#FFB0B0] w-full max-w-[1920px] mx-auto h-auto bg-[url(/category/bg_promotion.png)] bg-contain object-contain">
+      <div className="bg-[#FFB0B0] w-full h-auto bg-[url(/category/bg_promotion.png)] bg-contain object-contain">
         <Promotion />
       </div>
-      <div className="w-full max-w-[1920px] mx-auto">
+      <div>
         <Products />
       </div>
-      <div className="w-full max-w-[1920px] mx-auto h-auto bg-[#221692]">
+      <div className="w-full h-auto bg-[#221692]">
         <Footer />
-        <div className="w-full h-8 bg-white flex justify-center items-center text-[#4E4E4E] text-xs">
+        <div className="w-full h-8 fle bg-white flex justify-center items-center text-[#4E4E4E] text-xs">
           <CFooter />
           2023 JIB COMPUTER GROUP All rights reserved
         </div>
